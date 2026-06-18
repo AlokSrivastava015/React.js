d85ae00c0293513ec5bc26c98233bcbb6fa3abe9
alok0015 <alokji0015@gmail.com>
2026-06-18 14:06:00 +0530

    new

---
+++ 01reactproject/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./Chai"
+function App() {
+  return (
+    <div>
+   <Chai/>
+   <h1>Alok & Yogyata</h1>
+   </div>
+  )
+}
+
+export default App
---
+++ 01reactproject/src/Chai.jsx
@@ -0,0 +1,8 @@
+function Chai(){
+return (
+    <h1>ALok loves Yogyata And One Day Yogyata Loves Alok</h1>
+
+)
+}
+
+export default Chai
