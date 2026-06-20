de53fcd57c23c06eb71a191f0ea9917839cc3e92
alok0015 <alokji0015@gmail.com>
2026-06-20 15:58:41 +0530

    update

---
+++ 07ReactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/home/Home'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <header/>
+    <home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
